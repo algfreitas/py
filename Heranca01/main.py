7bed256bb365a0edf4c1b2a49a2259836f8097b5
André L G Freitas <andrelgfreitas@gmail.com>
2022-12-24 18:06:47 -0300

    add main

--- Heranca01/main.py
+++ Heranca01/main.py
@@ -4,5 +4,6 @@ e1 = Estudante('Laika', 321456987, 1)
 e2 = Estudante('Barba', 789654123, 2)
 e3 = Estudante('Berruga', 456987123, 3)
 
-print(e1.get_pessoa())
-print( e1.get_estudante())
+print(e1.get_pessoa(), e1.get_estudante())
+print(e2.get_pessoa(), e2.get_estudante())
+print(e3.get_pessoa(), e3.get_estudante())
